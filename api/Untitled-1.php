c6ea1ae12159ec1bbb1399602db4b5fc29c094b8
brandon <eeub18@bangor.ac.uk>
2019-03-05 20:02:14 +0000

    hanged stuff

--- api/Untitled-1.php
+++ api/Untitled-1.php
@@ -14,7 +14,7 @@
             $this->conn = null;
 
             try{
-                $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+                $this->conn = new PDO("mysql:host=" . $this->host . ";port=3306;dbname=" . $this->db_name, $this->username, $this->password);
                 $this->conn->exec("set names utf8");
 
             }catch(PDOException $exception){

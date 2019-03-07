8a27321b91c554e7f6b9d7fd379ac18231cae4d6
WYVERN <35181365+WYVERN2742@users.noreply.github.com>
2019-03-07 20:28:51 +0000

    Fix critical security flaw

--- api/config/database.php
+++ api/config/database.php
@@ -1,27 +1,27 @@
-<?php 
-    //used the and modified code from tutorial
-    class Database {
-        // specify your own database credentials
-        private $host = "localhost";
-        private $db_name = "testdb";
-        private $username = "teamrocketpi";
-        private $password = "teamrocket";
-        public $conn;
-        function __contruct(){
+<?php
+	//used the and modified code from tutorial
+	class Database {
+		// specify your own database credentials
+		private $host = "localhost";
+		private $db_name = "testdb";
+		private $username = "teamrocketpi";
+		private $password = "teamrocket";
+		public $conn;
+		function __contruct(){
 
-        }
-        public function openConnection(){
-            $this->conn = null;
+		}
+		public function openConnection(){
+			$this->conn = null;
 
-            try{
-                $this->conn = new PDO("mysql:host=" . $this->host . ";port=3306;dbname=" . $this->db_name, $this->username, $this->password);
-                $this->conn->exec("set names utf8");
+			try{
+				$this->conn = new PDO("mysql:host=" . $this->host . ";port=3306;dbname=" . $this->db_name, $this->username, $this->password);
+				$this->conn->exec("set names utf8");
 
-            }catch(PDOException $exception){
-                echo("Connection error: ". $exception->getMessage());
-            }
+			}catch(PDOException $exception){
+				echo("Connection error: ". $exception->getMessage());
+			}
 
-            return $this->conn;
-        }
-    }
+			return $this->conn;
+		}
+	}
 ?>

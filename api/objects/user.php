81c17aadca55609b84ff46ee3b87bbee16675161
Rory Macpherson <eeub40@bangor.ac.uk>
2019-03-11 16:59:06 +0000

    Fixed even bigger major security flaw

---
+++ api/objects/user.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Rory
+ * Date: 10/03/2019
+ * Time: 18:12
+ */
+
+class User {
+
+    private $conn;
+    private $table_name = "users";
+
+    public function __construct($conn) {
+        $this->conn = $conn;
+    }
+
+    public function login($id, $password) {
+        $this->conn->query("SELECT * FROM user WHERE user_id=:id");
+
+        //Maybe do something with $id?
+
+        $this->conn->bind(":id", $id);
+
+        $row = $this->conn->single();
+
+        if ($this->conn->rowCount() > 0) {
+            if (password_verify($password, $row["password"])) {
+                return $id;
+            }
+        }
+
+        return null;
+    }
+
+}

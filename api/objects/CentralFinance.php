894aff46bac8be27e169e2218079e1b1bd95d634
brandon <eeub18@bangor.ac.uk>
2019-04-04 23:35:41 +0100

    Removed error handling from central finance, so it can be put in the scripts instead. Added single result functions for user and budget codes

--- api/objects/CentralFinance.php
+++ api/objects/CentralFinance.php
@@ -21,84 +21,65 @@ class CentralFinance extends User {
 
     public function addUser($password, $firstName, $lastName, $role, $roomNo, $telephoneNo, $email) {
 
-        try{
-            $this->db->query("INSERT INTO User(password, firstName, lastName, role, roomNo, telephone, email) VALUES (:password, :firstName, :lastName, :role, :roomNo, :telephoneNo, :email)");
-            $this->db->bind(":password", $password); 
-            $this->db->bind(":firstName", $firstName);
-            $this->db->bind(":lastName", $lastName);
-            $this->db->bind(":role", $role);
-            $this->db->bind(":roomNo", $roomNo);
-            $this->db->bind(":telephoneNo", $telephoneNo);
+        $this->db->query("INSERT INTO User(password, firstName, lastName, role, roomNo, telephone, email) VALUES (:password, :firstName, :lastName, :role, :roomNo, :telephoneNo, :email)");
+        $this->db->bind(":password", $password); 
+        $this->db->bind(":firstName", $firstName);
+        $this->db->bind(":lastName", $lastName);
+        $this->db->bind(":role", $role);
+        $this->db->bind(":roomNo", $roomNo);
+        $this->db->bind(":telephoneNo", $telephoneNo);
 
-            $this->db->execute();
-        }catch(PDOException $e){
-            echo "Error: " . $e->getMessage();
-
-            return false;
-        }
-
+        $this->db->execute();
     }
 
     public function removeUser($userId) {
-        try{
-            $this->db->query("DELETE FROM User WHERE userId = :userId");
-            $this->db->bind(":userId", $userId);
-            
-            $this->db->execute();
-        }catch(PDOException $e){
-            echo "Error: " . $e->getMessage();
-
-            return false;
-        }
+        $this->db->query("DELETE FROM User WHERE userId = :userId");
+        $this->db->bind(":userId", $userId);
+        
+        $this->db->execute();
     }
 
     public function editUser($userId, $password, $firstName, $lastName, $role, $roomNo, $telephoneNo, $email) { //
-        try{
-            $this->db->query("UPDATE User SET password=:password, firstName=:firstName, lastName=:lastName, role=:role, roomNo=:roomNo, telephoneNo=:telephoneNo, email=:email WHERE userId=:userId");
-            $this->db->bind(":userId", $userId);
-            $this->db->bind(":password", $password);
-            $this->db->bind(":firstName", $firstName);
-            $this->db->bind(":lastName", $lastName);
-            $this->db->bind(":role", $role);
-            $this->db->bind(":roomNo", $roomNo);
-            $this->db->bind(":telephoneNo", $telephoneNo);
-            $this->db->bind(":email", $email);
+        $this->db->query("UPDATE User SET password=:password, firstName=:firstName, lastName=:lastName, role=:role, roomNo=:roomNo, telephoneNo=:telephoneNo, email=:email WHERE userId=:userId");
+        $this->db->bind(":userId", $userId);
+        $this->db->bind(":password", $password);
+        $this->db->bind(":firstName", $firstName);
+        $this->db->bind(":lastName", $lastName);
+        $this->db->bind(":role", $role);
+        $this->db->bind(":roomNo", $roomNo);
+        $this->db->bind(":telephoneNo", $telephoneNo);
+        $this->db->bind(":email", $email);
 
-            $this->db-execute();
-
-        }catch(PDOException $e){
-            echo "Error: " . $e->getMessage();
-        }
+        $this->db-execute();
     }
 
     public function createBudgetCode($budgetCode, $ownerId, $procurementOfficer) {
-        try{
-            $this->db->query("INSERT INTO BudgetCode(budgetCode, ownerId, procurementOfficer) VALUES (:budgetCode, :ownerId, :procurementOfficer)");
-            $this->db->bind(":budgetCode", $budgetCode);
-            $this->db->bind(":ownerId", $ownerId);
-            $this->db->bind(":procurementOfficer", $procurementOfficer);
+        $this->db->query("INSERT INTO BudgetCode(budgetCode, ownerId, procurementOfficer) VALUES (:budgetCode, :ownerId, :procurementOfficer)");
+        $this->db->bind(":budgetCode", $budgetCode);
+        $this->db->bind(":ownerId", $ownerId);
+        $this->db->bind(":procurementOfficer", $procurementOfficer);
 
-            $this->db->execute();
-        }catch(PDOException $e){
-            echo "Error: " . $e->getMessage();
-        }
+        $this->db->execute();
     }
 
     public function updateBudgetCode($ownerId, $procurementOfficer){
-        try{
-            $this->db->query("UPDATE BudgetCode SET ownerId=:ownerId, procurementOfficer=:procurementOfficer WHERE budgetCode=:budgetCode");
-            $this->db->bind(":ownerId", $ownerId);
-            $this->db->bind(":procurementOfficer", $procurementOfficer);
+        $this->db->query("UPDATE BudgetCode SET ownerId=:ownerId, procurementOfficer=:procurementOfficer WHERE budgetCode=:budgetCode");
+        $this->db->bind(":ownerId", $ownerId);
+        $this->db->bind(":procurementOfficer", $procurementOfficer);
 
-            $this->db->execute();
-        }catch(PDOException $e){
-            echo "Error: " . $e->getMessage();
-        }
+        $this->db->execute();
     }
 
     public function viewUsers() {
         $this->db->query("SELECT userId, firstName, lastName, role, roomNo, telephoneNo, email FROM User"); //all users in the database.
 		$rs = $this->db->resultSet();
+		return $rs;
+    }
+
+    public function viewUser($userId){//would be better to combine this with the method above.
+        $this->db->query("SELECT userId, firstName, lastName, role, roomNo, telephoneNo, email FROM User WHERE userId=:userId"); //all users in the database.
+        $this->db->bind(":userId", $userId);
+        $rs = $this->db->resultSet();
 		return $rs;
     }
 
@@ -108,6 +89,11 @@ class CentralFinance extends User {
         return $rs;
     }
 
+    public function viewBudgetcode($budgetCode){
+        $this->db->query("SELECT budgetCode, ownerId, procurementOfficer FROM BudgetCode WHERE budgetCode=:budgetCode");
+        $this->db->bind(":budgetCode", $budgetCode);
+        $rs = $this->db->resultSet();
+        return $rs;
+    }
 
-    //might need to add remove budget code function
 }

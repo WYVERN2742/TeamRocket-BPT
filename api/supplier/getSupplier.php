6b3422070ad2c202f39ae11322faa83449a06526
WYVERN <35181365+WYVERN2742@users.noreply.github.com>
2019-04-24 19:49:16 +0100

    update php includes

--- api/supplier/getSupplier.php
+++ api/supplier/getSupplier.php
@@ -5,8 +5,8 @@ header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-include_once 'config/Database.php';
-include_once 'objects/User.php';
+include_once '../config/Database.php';
+include_once '../objects/User.php';
 
 session_start();
 
